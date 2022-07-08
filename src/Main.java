71db621016471c050d19c76a97eea784cfc16c2f
BoomStickk <6endgame6@gmail.com>
2022-07-08 03:47:19 +0300

    almost done

--- src/Main.java
+++ src/Main.java
@@ -20,31 +20,30 @@ public class Main {
                     System.out.println("Average domestic height is about 25sm");
                     result = yourHeight / 0.25;
                     System.out.println("So I guess you are about " + String.format("%.2f", result) + " little puffy cats tall");
-                    System.out.println("Do you wanna continue having fun: ");
-                    preciousChoiceYN = in.next();
-                    if (preciousChoiceYN.equals("y")) {
-                        System.out.println("OK than");
-                        continue;
-                    } else if (preciousChoiceYN.equals("n")) {
-                        System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                        break;
+                    System.out.println();
+                    System.out.println("Press enter to continue");
+                    try {
+
+                        //noinspection ResultOfMethodCallIgnored
+                        System.in.read();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
-                    break;
                 } else if (preciousChoice3 == 2) {
                     System.out.println("Average Puma height is 60-75");
                     result = yourHeight / 0.675;
                     System.out.println("So I guess you are about " + String.format("%.2f", result) + " scary pumas tall");
-                    System.out.println("Do you wanna continue having fun: ");
-                    preciousChoiceYN = in.next();
-                    if (preciousChoiceYN.equals("y")) {
+                    System.out.println();
+                    System.out.println("Press enter to continue");
+                    try {
 
-                    } else if (preciousChoiceYN.equals("n")) {
-                        System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                        break;
+                        //noinspection ResultOfMethodCallIgnored
+                        System.in.read();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
                 } else if (preciousChoice3 == 3) {
-                    System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit\n ");
-                    preciousChoice = in.nextInt();
+                    preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit\n ", in);
                 }
             } else if (preciousChoice == 2) {
                 System.out.println("Choose big cat family: \n1:Genus Panthera (big, or roaring, cats)\n2:Some of the rest I taught are graceful as you\n3:Back");
@@ -52,36 +51,43 @@ public class Main {
                 if (yetAnotherPreciousChoice == 1) {
                     System.out.println("Just choose tiger or lion, thought I envision you as more of a lioness :D, after all you are a LEO right");
                     preciousChoice2 = in.next();
-                    if (preciousChoice2.equals("lion")) {
-                        System.out.println("Average lion height is 90-110");
-                        result = yourHeight;
-                        System.out.println("So I guess you are about " + String.format("%.2f", result) + " big hungry cats tall");
-                        System.out.println("Do you wanna continue having fun: ");
-                        preciousChoiceYN = in.next();
-                        if (preciousChoiceYN.equals("y")) {
-                        } else if (preciousChoiceYN.equals("n")) {
-                            System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                            break;
-                        }
-                    } else if (preciousChoice2.equals("tiger")) {
-                        System.out.println("Average tiger height is 80-110");
-                        result = yourHeight / 0.95;
-                        System.out.println("So I guess you are about " + String.format("%.2f", result) + " great striped cats tall");
-                        System.out.println("Do you wanna continue having fun: ");
-                        preciousChoiceYN = in.next();
-                        if (preciousChoiceYN.equals("y")) {
+                    switch (preciousChoice2) {
+                        case "lion" -> {
+                            System.out.println("Average lion height is 90-110");
+                            result = yourHeight;
+                            System.out.println("So I guess you are about " + String.format("%.2f", result) + " big hungry cats tall");
+                            System.out.println();
+                            System.out.println("Press enter to continue");
+                            try {
 
-                        } else if (preciousChoiceYN.equals("n")) {
-                            System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                            break;
+                                //noinspection ResultOfMethodCallIgnored
+                                System.in.read();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
                         }
-                    } else if (preciousChoice2.equals("LEO")) {
-                        result=yourHeight/1.77;
-                        System.out.println("You found my secret menu,good job.");
-                        System.out.println("   |\\---/|\n   | ,_, |\n    \\_`_/-..----.\n ___/ `   ' ,\"\"+ \\ \n(__...'   __\\    |`.___.';\n  (_,...'(_,.`__)/'.....+");
-                        System.out.println("Well I'm 1.77(above average) witch means I am 12sm higher than you :D\nThat makes you "+ String.format("%.2f", result) +" fun guys tall.\nThat means you are SHORT :D and yet you are very ELEGANT and BEAUTIFUL.\nI guess that works for your daily beauty confirmation <3\nHope you had fun. ");
+                        case "tiger" -> {
+                            System.out.println("Average tiger height is 80-110");
+                            result = yourHeight / 0.95;
+                            System.out.println("So I guess you are about " + String.format("%.2f", result) + " great striped cats tall");
+                            System.out.println();
+                            System.out.println("Press enter to continue");
+                            try {
 
-                    }break;
+                                //noinspection ResultOfMethodCallIgnored
+                                System.in.read();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        case "LEO" -> {
+                            result = yourHeight / 1.77;
+                            System.out.println("You found my secret menu,good job.");
+                            System.out.println("   |\\---/|\n   | ,_, |\n    \\_`_/-..----.\n ___/ `   ' ,\"\"+ \\ \n(__...'   __\\    |`.___.';\n  (_,...'(_,.`__)/'.....+");
+                            System.out.println("Well I'm 1.77(above average) witch means I am 12sm higher than you :D\nThat makes you " + String.format("%.2f", result) + " fun guys tall.\nThat means you are SHORT :D and yet you are very ELEGANT and BEAUTIFUL.\nI guess that works for your daily beauty confirmation <3\nHope you had fun. ");
+                        }
+                    }
+                    break;
 
                 } else if (yetAnotherPreciousChoice == 2) {
                     System.out.println("choose 1 of those:\n1:Marbled cat\n2:Snow leopard");
@@ -90,29 +96,32 @@ public class Main {
                         System.out.println("Average marbled cat height is 30");
                         result = yourHeight / 0.3;
                         System.out.println("So I guess you are about " + String.format("%.2f", result) + " little cute cats tall");
-                        System.out.println("Do you wanna continue having fun: ");
-                        preciousChoiceYN = in.next();
-                        if (preciousChoiceYN.equals("y")) {
-                        } else if (preciousChoiceYN.equals("n")) {
-                            System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                            break;
+                        System.out.println();
+                        System.out.println("Press enter to continue");
+                        try {
+
+                            //noinspection ResultOfMethodCallIgnored
+                            System.in.read();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
 
                     } else if (preciousChoice == 2) {
                         System.out.println("Average snow leopard height is 60");
                         result = yourHeight / 0.6;
                         System.out.println("So I guess you are about " + String.format("%.2f", result) + " cold cats tall");
-                        System.out.println("Do you wanna continue having fun: ");
-                        preciousChoiceYN = in.next();
-                        if (preciousChoiceYN.equals("y")) {
-                        } else if (preciousChoiceYN.equals("n")) {
-                            System.out.println("Did you already see all the stupid stuff I did K than bye later");
-                            break;
+                        System.out.println();
+                        System.out.println("Press enter to continue");
+                        try {
+
+                            //noinspection ResultOfMethodCallIgnored
+                            System.in.read();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
                     }
                 } else if (yetAnotherPreciousChoice == 3) {
-                    System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit\n ");
-                    preciousChoice = in.nextInt();
+                    preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit\n ", in);
                 }
             } else if (preciousChoice == 3) {
 
@@ -127,69 +136,67 @@ public class Main {
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
-                System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ");
-                preciousChoice = in.nextInt();
+                preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ", in);
             } else if (preciousChoice == 4) {
                 exitCounter++;
                 switch (exitCounter) {
-                    case 1:
+                    case 1 -> {
                         System.out.print("Are you sure you want to exit: ");
-
                         preciousChoiceYN = in.next();
                         if (preciousChoiceYN.equals("y")) {
                             continue;
                         } else if (preciousChoiceYN.equals("n")) {
-                            System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ");
-                            preciousChoice = in.nextInt();
+                            preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ", in);
 
                         }
                         continue;
-
-                    case 2:
+                    }
+                    case 2 -> {
                         System.out.print("I will be fun and tell you, you should continue...or may by you cant be bothered anymore ?: ");
-
                         preciousChoiceYN = in.next();
                         if (preciousChoiceYN.equals("y")) {
                             continue;
                         } else if (preciousChoiceYN.equals("n")) {
-                            System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ");
-                            preciousChoice = in.nextInt();
+                            preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ", in);
 
                         }
                         continue;
-                    case 3:
+                    }
+                    case 3 -> {
                         System.out.print("please don't exit Im sure there is something you are missing...got to go?: ");
-
                         preciousChoiceYN = in.next();
                         if (preciousChoiceYN.equals("y")) {
                             continue;
                         } else if (preciousChoiceYN.equals("n")) {
-                            System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ");
-                            preciousChoice = in.nextInt();
+                            preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ", in);
 
                         }
                         continue;
-                    case 4:
+                    }
+                    case 4 -> {
                         System.out.print("You are definitely a LEO you can't be reasoned with(may be I left a hint around here)...still want to leave: ");
-
                         preciousChoiceYN = in.next();
                         if (preciousChoiceYN.equals("y")) {
                             continue;
                         } else if (preciousChoiceYN.equals("n")) {
-                            System.out.print("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ");
-                            preciousChoice = in.nextInt();
+                            preciousChoice = getPreciousChoice("FAMILY FELIDAE(This mean cats I guess): \nChoose subfamily: \n1:Subfamily Felinae\n2:Subfamily Pantherinae\n3:Help\n4:Exit ", in);
 
                         }
                         continue;
-                    case 5:
-                        System.out.print("OK OK I guess you are done, hope you had fun");
-                        break;
+                    }
+                    case 5 -> System.out.print("OK OK I guess you are done, hope you had fun");
                 }
                 break;
 
 
             }
         }
+    }
+
+    private static int getPreciousChoice(String s, Scanner in) {
+
+        System.out.print(s);
+        return in.nextInt();
     }
 
 }
